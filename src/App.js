06a5bf19f8ec9df5154429a0af40cdd2daebbe55
LIJOMLOYID <lijomloyid@gmail.com>
2020-12-23 16:21:09 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/ui/Header'
 import './App.css';
 
 const App = () => {
   return (
     <div className="container">
-      Hello
+      <Header/>
     </div>
   );
 }
